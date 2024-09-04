7924611541b5f098246d157aa6763cc898348515
Gafall <thomasaribech@gmail.com>
2024-09-04 05:52:25 +0000

    added a function that counts all the words in a book

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def word_counter(book):
+    count = len(book.split())
+    return count
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        print(word_counter(file_contents))
 
 main()
