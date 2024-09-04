ae6bcaf0932d8790a3b92898c9be540b9b7a43a5
Gafall <thomasaribech@gmail.com>
2024-09-04 06:28:08 +0000

    now prints a report on the book

--- main.py
+++ main.py
@@ -18,11 +18,32 @@ def character_counter(book):
 
     return character_count
 
+def convert_character_dict_to_list_of_dict(dict):
+    lst = []
+    for key in dict:
+        lst.append({"char": key, "num": dict[key]})
+    return lst
+
+def sort_on(dict):
+    return dict["num"]
+
+def print_report(path, word_count, character_list):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+    for char in character_list:
+        if char["char"].isalpha():
+            print(f"The '{char['char']}' character was found {char['num']} times")
+    print("--- End report ---")
+
 
 def main():
     frankenstein_book_path = "books/frankenstein.txt"
     frankenstein_book = read_file_contents(frankenstein_book_path)
     word_count = word_counter(frankenstein_book)
-    print(f"Frankenstein has {word_count} words")
-    print(character_counter(frankenstein_book))
+    character_dict = character_counter(frankenstein_book)
+    character_list = convert_character_dict_to_list_of_dict(character_dict)
+    character_list.sort(reverse=True, key=sort_on)
+    print_report(frankenstein_book_path, word_count, character_list)
+
 main()
